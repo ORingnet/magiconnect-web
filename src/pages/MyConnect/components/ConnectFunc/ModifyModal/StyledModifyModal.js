80370231d1f309395685306da6c469e00187cad7
4N <aiko3310@hotmail.com>
2020-05-18 10:23:55 +0800

    整理 0506 報價單的檔案

--- src/pages/MyConnect/components/ConnectFunc/ModifyModal/StyledModifyModal.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { Input } from 'reactstrap';
-const StyledInput = styled(Input)`
-  &::placeholder {
-    color: #c7c7c7;
-  }
-`;
-export { StyledInput };
